e3342e1c9e28d919757a8e9ed402be026718fd28
kwabena <kwabena@mpharma.com>
2021-02-03 14:28:40 +0000

    Handle keyboard interrupt error

--- zebrok/worker.py
+++ zebrok/worker.py
@@ -1,8 +1,8 @@
 import zmq
 import json
-from connection import SocketConnection
-from utils import unpickle_task
-from registry import TaskRegistry
+from .connection import SocketConnection
+from .utils import unpickle_task
+from .registry import TaskRegistry
 
 
 class Worker(object):
@@ -12,11 +12,13 @@ class Worker(object):
         self.tasks.register(task)
 
     def start(self):
-        sock = SocketConnection().connect_to_socket()
-
-        while True:
-            message = sock.recv()
-            payload = unpickle_task(message)
-            func = self.tasks.get(payload["task"])
-            kwargs = payload["kwargs"]
-            func(**kwargs)
+        try:
+            sock = SocketConnection().connect_to_socket()
+            while True:
+                message = sock.recv()
+                payload = unpickle_task(message)
+                func = self.tasks.get(payload["task"])
+                kwargs = payload["kwargs"]
+                func(**kwargs)
+        except KeyboardInterrupt:
+            sock.close()
