ca3fa0fce6ee750797b0271bd0c942e530e3d5b2
kwabena <kwabena@mpharma.com>
2021-02-04 22:20:30 +0000

    Remove unsued imports

--- zebrok/worker.py
+++ zebrok/worker.py
@@ -1,5 +1,3 @@
-import zmq
-import json
 from .connection import SocketConnection
 from .utils import unpickle_task
 from .registry import TaskRegistry
@@ -8,17 +6,32 @@ from .registry import TaskRegistry
 class Worker(object):
     tasks = TaskRegistry()
 
+    def __init__(self):
+        self.sock = SocketConnection().connect_to_socket()
+
     def register(self, task):
+        """
+        Registers tasks to in-memory task registry
+        """
         self.tasks.register(task)
 
     def start(self):
+        """
+        Starts worker to be reciving incoming
+        messages
+        """
         try:
-            sock = SocketConnection().connect_to_socket()
             while True:
-                message = sock.recv()
+                message = self.sock.recv()
                 payload = unpickle_task(message)
                 func = self.tasks.get(payload["task"])
                 kwargs = payload["kwargs"]
                 func(**kwargs)
         except KeyboardInterrupt:
-            sock.close()
+            self.stop()
+
+    def stop(self):
+        """
+        Closes socket connection
+        """
+        self.sock.close()
