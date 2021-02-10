31262eda69e5566283df14b975fb0ec80fcdd645
kwabena <kwabena@mpharma.com>
2021-02-10 00:01:21 +0000

    Improve documenting on functions

--- zebrok/discovery.py
+++ zebrok/discovery.py
@@ -4,4 +4,7 @@ tasks = importlib.import_module("tasks", package='zebrok')
 
 
 def get_discovered_task_by_name(task_name):
+    '''
+    Finds declared tasks
+    '''
     return getattr(tasks, task_name)
