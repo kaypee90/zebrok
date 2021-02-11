66daa1651bc42f65a2e46c76f47bf87c59657cba
kwabena <kwabena@mpharma.com>
2021-02-11 01:50:47 +0000

    Add more tests to task

--- tests.py
+++ tests.py
@@ -4,7 +4,15 @@ from zebrok.utils import (get_publisher_port_and_host,
 from zebrok.registry import TaskRegistry
 from zebrok import app
 from zebrok.worker import Worker
+from zebrok.discovery import get_discovered_task_by_name
 
+class TestDiscovery(unittest.TestCase):
+
+    def test_get_discovered_task_by_name(self):
+        expected_discovered_task_name = "long_running_task_one"
+        func = get_discovered_task_by_name(expected_discovered_task_name)
+        actual_discovered_task_name = func.get_task_object().__name__
+        self.assertEqual(expected_discovered_task_name, actual_discovered_task_name)
 
 class TestUtils(unittest.TestCase):
 
