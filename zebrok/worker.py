4f64424b9f0a1c7fde008c68e3011f19900367e6
kwabena <kwabena@mpharma.com>
2021-02-12 18:32:18 +0000

    Refactor worker

--- zebrok/worker.py
+++ zebrok/worker.py
@@ -8,17 +8,17 @@ logger = create_logger(__name__)
 
 class TaskRunner(object):
 
-    def __init__(self, task_registry, auto_discover=False):
+    def __init__(self, auto_discover=False):
         self.auto_discover = auto_discover
-        self.task_registry = task_registry
 
-    def find_and_execute_task(self, task_name, **kwargs):
+    def find_and_execute_task(self, task_registry, task_name, **kwargs):
         '''
         Finds and execute tasks
         '''
-        func = self.task_registry.get(task_name)
-        if func:
-            func(**kwargs)
+        if task_registry:
+            func = task_registry.get(task_name)
+            if func:
+                func(**kwargs)
 
         if not func and self.auto_discover:
             func = get_discovered_task_by_name(task_name)
@@ -34,7 +34,7 @@ class Worker(object):
         self.auto_discover = auto_discover
         self.sock, self.context = SocketConnection.initialize(bind_to_socket=False)
         self.tasks = task_registry if task_registry else InMemoryTaskRegistry()
-        self.runner = task_runner if task_runner else TaskRunner(self.tasks, auto_discover)
+        self.runner = task_runner if task_runner else TaskRunner(auto_discover)
 
     def register_task(self, task):
         """
@@ -53,7 +53,7 @@ class Worker(object):
                 task_name = message.pop("task")
                 kwargs = message.pop("kwargs")
                 logger.info(f"received task: {task_name}")
-                self.runner.find_and_execute_task(task_name, **kwargs)
+                self.runner.find_and_execute_task(self.tasks, task_name, **kwargs)
         except KeyboardInterrupt:
             self.stop()
 
