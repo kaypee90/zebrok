377ff369a40cd066f77d4cbfec7ccd3fd5bb2a22
kwabena <kwabena@mpharma.com>
2021-02-13 01:23:59 +0000

    Add running of multiple workers

--- zebrok/worker.py
+++ zebrok/worker.py
@@ -1,24 +1,27 @@
-from .connection import SocketConnection
+import concurrent.futures
+from .connection import SocketType, ZmqBindConnection, ZmqConnectTypeConnection
 from .registry import InMemoryTaskRegistry
 from .logging import create_logger
 from .discovery import get_discovered_task_by_name
+from .utils import get_publisher_port_and_host
 
 logger = create_logger(__name__)
 
 
+
 class TaskRunner(object):
 
-    def __init__(self, auto_discover=False):
+    def __init__(self, task_registry, auto_discover=False):
         self.auto_discover = auto_discover
+        self.registry = task_registry
 
-    def find_and_execute_task(self, task_registry, task_name, **kwargs):
+    def find_and_execute_task(self, task_name, **kwargs):
         '''
         Finds and execute tasks
         '''
-        if task_registry:
-            func = task_registry.get(task_name)
-            if func:
-                func(**kwargs)
+        func = self.registry.get(task_name)
+        if func:
+            func(**kwargs)
 
         if not func and self.auto_discover:
             func = get_discovered_task_by_name(task_name)
@@ -28,13 +31,59 @@ class TaskRunner(object):
             logger.error("Task not found!")
 
 
+class GenericWorker(object):
+
+    def __init__(self, connection, runner):
+        self.slaves = []
+        self.connection = connection
+        self.socket = self.connection.socket
+        self.runner = runner
+        self.current_slave = 0
+
+    def _execute_worker(self):
+        logger.info(f"starting worker on: {self.connection.socket_address}")
+        try:
+            while True:
+                message = self.socket.recv_json()
+                if self.number_of_slaves > 0:
+                    logger.info("sending task to slave worker")
+                    push_socket = self.slaves[self.current_slave]
+                    push_socket.send_json(message)
+                    self.increment_current_slave()
+                else:
+                    task_name = message.pop("task")
+                    kwargs = message.pop("kwargs")
+                    logger.info(f"received task: {task_name}")
+                    self.runner.find_and_execute_task(task_name, **kwargs)
+        except KeyboardInterrupt:
+            self.stop()
+
+    def start(self):
+        self._execute_worker()
+
+    @property
+    def number_of_slaves(self):
+        return len(self.slaves)
+
+    def increment_current_slave(self):
+        self.current_slave += 1
+        if self.current_slave == self.number_of_slaves:
+            self.current_slave = 0
+
+    def stop(self):
+        self.current_slave = 0
+        self.connection.close()
+
+    def add_slave(self, worker):
+        self.slaves.append(worker)
+    
+
 class Worker(object):
 
-    def __init__(self, auto_discover=False, task_runner=None, task_registry=None):
-        self.auto_discover = auto_discover
-        self.sock, self.context = SocketConnection.initialize(bind_to_socket=False)
+    def __init__(self, number_of_slaves=0, auto_discover=False, task_registry=None):
         self.tasks = task_registry if task_registry else InMemoryTaskRegistry()
-        self.runner = task_runner if task_runner else TaskRunner(auto_discover)
+        self.runner = TaskRunner(self.tasks, auto_discover)
+        self.number_of_slaves = number_of_slaves
 
     def register_task(self, task):
         """
@@ -42,25 +91,28 @@ class Worker(object):
         """
         self.tasks.register(task)
 
+    def _execute_worker(self):
+        port, host = get_publisher_port_and_host()
+        max_workers = self.number_of_slaves + 1
+        master_socket = ZmqBindConnection(SocketType.ZmqPull, host, port)
+        master_worker = GenericWorker(master_socket, self.runner)
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+            for i in range(self.number_of_slaves):
+                slave_port = port + i + 1
+                push_socket = ZmqBindConnection(SocketType.ZmqPush, host, slave_port, master_socket.context)
+                master_worker.add_slave(push_socket.socket)
+                
+                pull_socket_ = ZmqConnectTypeConnection(SocketType.ZmqPull, host, slave_port)
+                slave_worker = GenericWorker(pull_socket_, self.runner)
+                executor.submit(slave_worker.start)
+
+            executor.submit(master_worker.start)
+
     def start(self):
         """
         Starts worker to be receiving incoming
         messages
         """
-        try:
-            while True:
-                message = self.sock.recv_json()
-                task_name = message.pop("task")
-                kwargs = message.pop("kwargs")
-                logger.info(f"received task: {task_name}")
-                self.runner.find_and_execute_task(self.tasks, task_name, **kwargs)
-        except KeyboardInterrupt:
-            self.stop()
-
-    def stop(self):
-        """
-        Closes socket connection
-        """
-        self.sock.close()
-        self.context.term()
-
+        self._execute_worker()
+        
