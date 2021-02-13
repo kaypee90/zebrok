f51f6647007b54a824add9427e09fb8cb4d516c3
kwabena <kwabena@mpharma.com>
2021-02-13 02:00:05 +0000

    Clean up code

--- zebrok/discovery.py
+++ zebrok/discovery.py
@@ -1,10 +1,10 @@
 import importlib
 
-tasks = importlib.import_module("tasks", package='zebrok')
+tasks = importlib.import_module("tasks", package="zebrok")
 
 
 def get_discovered_task_by_name(task_name):
-    '''
+    """
     Finds declared tasks
-    '''
+    """
     return getattr(tasks, task_name)
--- zebrok/registry.py
+++ zebrok/registry.py
@@ -3,10 +3,10 @@ from abc import ABC, abstractmethod
 
 
 class BaseTaskRegistry(ABC):
-    '''
+    """
     Concrete task registry types must
     inherit this absract class
-    '''
+    """
 
     @abstractmethod
     def register(self, task):
@@ -23,14 +23,14 @@ class InMemoryTaskRegistry(BaseTaskRegistry, dict):
     """
 
     def register(self, task):
-        '''
+        """
         Adds a task to in-memory registry
-        '''
+        """
         task = inspect.isclass(task) and task() or task
         self[task.get_task_object().__name__] = task
 
     def unregister(self, name):
-        '''
+        """
         Removes a task to in-memory registry
-        '''
+        """
         self.pop(getattr(name, "name", name))
