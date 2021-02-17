153892b116a1f59e78786d23b09eb3575857732d
kwabena <kwabena@mpharma.com>
2021-02-17 23:03:48 +0000

    Fix broken tests

--- tests/tests.py
+++ tests/tests.py
@@ -5,18 +5,77 @@ import unittest
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
 from zebrok.utils import get_worker_port_and_host
-from zebrok.registry import InMemoryTaskRegistry
+from zebrok.registry import InMemoryTaskRegistry, RegistryFactory, RegistryType
 from zebrok import app
-from zebrok.worker import WorkerInitializer
+from zebrok.worker import WorkerInitializer, TaskRunner
+from zebrok.logging import create_logger
 from zebrok.discovery import get_discovered_task_by_name
+from zebrok.connection import (
+    ConnectionType,
+    ConnectionFactory,
+    ZmqConnectTypeConnection,
+    ZmqBindConnection,
+    SocketType,
+)
+
+
+class TestLoggingSetup(unittest.TestCase):
+    def test_create_logger(self):
+        logger = create_logger(__name__)
+        self.assertIsNotNone(logger)
+
+
+class TestTaskRunner(unittest.TestCase):
+    def setUp(self):
+        def say_hello(name):
+            print(f"Hello to you {name}")
+
+        self.registry = {"say_hello": say_hello}
+
+    def test_run_registered_task(self):
+        runner = TaskRunner(self.registry, True)
+        is_executed = runner.find_and_execute_task("say_hello", **{"name": "John"})
+        self.assertTrue(is_executed)
+
+    def test_run_non_registered_task(self):
+        runner = TaskRunner(self.registry, True)
+        is_executed = runner.find_and_execute_task("print_name", **{"name": "Sam"})
+        self.assertFalse(is_executed)
 
 
 class TestConnectionFactory(unittest.TestCase):
-    pass
+    def test_create_zmq_connect_type(self):
+        connection_type = ConnectionType.zmq_connect
+        settings = (
+            SocketType.ZmqPull,
+            "localhost",
+            7890,
+        )
+        connection = ConnectionFactory.create_connection(connection_type, *settings)
+        self.assertIsInstance(connection, ZmqConnectTypeConnection)
+        self.assertEqual(connection.socket_address, "tcp://127.0.0.1:7890")
+        self.assertEqual(connection.socket_type, SocketType.ZmqPull.value)
+        connection.close()
+
+    def test_create_zmq_bind_type(self):
+        connection_type = ConnectionType.zmq_bind
+        settings = (
+            SocketType.ZmqPush,
+            "localhost",
+            7891,
+        )
+        connection = ConnectionFactory.create_connection(connection_type, *settings)
+        self.assertIsInstance(connection, ZmqBindConnection)
+        self.assertEqual(connection.socket_address, "tcp://127.0.0.1:7891")
+        self.assertEqual(connection.socket_type, SocketType.ZmqPush.value)
+        connection.close()
 
 
 class TestRegistryFactory(unittest.TestCase):
-    pass
+    def test_create_registry(self):
+        in_memory_registry_type = RegistryType.in_memory
+        registry = RegistryFactory.create_registry(in_memory_registry_type)
+        self.assertIsInstance(registry, InMemoryTaskRegistry)
 
 
 class TestDiscovery(unittest.TestCase):
@@ -35,7 +94,7 @@ class TestUtils(unittest.TestCase):
         self.assertEqual(host, "localhost")
 
 
-class TestRegistry(unittest.TestCase):
+class TestInMemoryRegistry(unittest.TestCase):
     def setUp(self):
         self.registry = InMemoryTaskRegistry()
 
