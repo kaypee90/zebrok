ad5d78ac4c157ad7d1ff75efaa17e420d76c182f
kwabena <kwabena@mpharma.com>
2021-02-23 12:35:36 +0000

    Remove django specific config implementation

--- zebrok/utils.py
+++ zebrok/utils.py
@@ -1,5 +1,6 @@
 import os
 from .logging import create_logger
+from .config import WORKER_PORT, WORKER_HOST
 
 logger = create_logger(__name__)
 
@@ -9,35 +10,7 @@ def get_worker_port_and_host():
     Retrieves port number and the host worker will
     be listening from configuration
     """
-    port = None
-    host = None
-
-    try:
-        from django.conf import settings
-
-        if not port:
-            port = settings.WORKER_PORT
-
-        if not host:
-            host = settings.WORKER_HOST
-    except Exception:
-        logger.warning("Attempt to load from django settings failed!")
-
-    if not port:
-        port = os.environ.get("WORKER_PORT")
-
-    if not host:
-        host = os.environ.get("WORKER_HOST")
-
-    # Load default values
-    if not port:
-        from .config import WORKER_PORT
-
-        port = WORKER_PORT
-
-    if not host:
-        from .config import WORKER_HOST
-
-        host = WORKER_HOST
+    port = os.environ.get("WORKER_PORT", WORKER_PORT)
+    host = os.environ.get("WORKER_HOST", WORKER_HOST)
 
     return int(port), host
