cedb25827b6368e0101307e5b2700ccf569191c5
kaypee90 <kaypee90@yahoo.com>
2021-05-09 18:13:10 +0000

    Add doc string to custom exception class

--- zebrok/exceptions.py
+++ zebrok/exceptions.py
@@ -1,2 +1,6 @@
 class ZebrokNotImplementedError(NotImplementedError):
+    """
+    Custom exception to be thrown when a derived class
+    fails to implement an abstract method of a base class
+    """
     pass
