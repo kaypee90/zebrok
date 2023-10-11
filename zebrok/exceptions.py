93e54cd8c805d4a762cadc9a89f90c13bf3647c2
kaypee90 <kaypee90@yahoo.com>
2023-10-11 22:42:29 +0000

    Format exceptions.py file

--- zebrok/exceptions.py
+++ zebrok/exceptions.py
@@ -3,4 +3,5 @@ class ZebrokNotImplementedError(NotImplementedError):
     Custom exception to be thrown when a derived class
     fails to implement an abstract method of a base class
     """
+
     pass
