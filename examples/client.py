bf1addff4cd3aa2344738612c5e8c412b05aee9a
kaypee90 <kaypee90@yahoo.com>
2024-05-01 17:06:52 +0000

    Replace black with ruff formatter

--- examples/client.py
+++ examples/client.py
@@ -1,9 +1,11 @@
+# ruff-disable-check
 """
 Example file showing how a registered task
 can be called and executed on a zebrok worker
 """
 import examples
-from tasks import long_running_task_one, long_running_task_two
+from tasks import long_running_task_one
+from tasks import long_running_task_two
 
 long_running_task_one.run(firstname="Kay", lastname="Pee")
 long_running_task_two.run(email="samplemail@mail.com")
