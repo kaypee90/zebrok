94c6db15609affeb775ae09eb4693ec134e6f17e
Kwabena <kaypee90@yahoo.com>
2024-07-27 15:50:42 +0000

    improve logging configuration (#96)

--- zebrok/logging.py
+++ zebrok/logging.py
@@ -1,20 +1,27 @@
 import logging
 
 
-def create_logger(module_name: str) -> logging.Logger:
+def create_logger(module_name: str) -> "logging.Logger":
     """
     Initializes a new logger object
     parameters:
         module_name (str): name of module importing the logger
     """
-
-    logging.basicConfig(
-        level=logging.DEBUG,
-        format="%(asctime)s %(name)s %(levelname)s:%(message)s",
-    )
-    log_handler = logging.StreamHandler()
-    log_file_handler = logging.FileHandler("zebrok.log")
     logger = logging.getLogger(module_name)
-    logger.addHandler(log_handler)
-    logger.addHandler(log_file_handler)
+    logger.setLevel(logging.DEBUG)
+
+    stream_handler = logging.StreamHandler()
+    file_handler = logging.FileHandler("zebrok.log")
+
+    logger.addHandler(stream_handler)
+    logger.addHandler(file_handler)
+
+    formatter = logging.Formatter(
+        "{asctime} - {name} - {levelname} - {message}",
+        style="{",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+    stream_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
     return logger
